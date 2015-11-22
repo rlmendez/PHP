3af13de6cb964abb3f7438981957dea9acaa8e83
rlmendez <marcoamendez@uan.edu.co>
2015-11-22 14:46:14 -0500

    1.1

--- conexion.php
+++ conexion.php
@@ -1,9 +1,4 @@
 <?php 
-	//$host = "ec2-107-21-219-235.compute-1.amazonaws.com";
-	//$database = "d51gev90fgshgo";
-	//$user = "bvwqtbbjrfhckx";
-	//$puerto = "5432";
-	//$contrasena = "l_LEjFRRGMhvMzDErHwQyY_a1n";
 	//$url = "postgres://bvwqtbbjrfhckx:l_LEjFRRGMhvMzDErHwQyY_a1n@ec2-107-21-219-235.compute-1.amazonaws.com:5432/d51gev90fgshgo"
 	$conn_string = "host=ec2-107-21-219-235.compute-1.amazonaws.com port=5432 dbname=d51gev90fgshgo user=bvwqtbbjrfhckx password=l_LEjFRRGMhvMzDErHwQyY_a1n";
 	$conexion = pg_connect($conn_string);
